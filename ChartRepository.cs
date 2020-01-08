67dbe39b1e443e9ec21690035cb14ce815b790aa
Titanic <telefonefix@hotmail.com>
2020-01-08 22:42:36 +0100

    Add files via upload

---
+++ ChartRepository.cs
@@ -0,0 +1,67 @@
+﻿using Domain.SharedKernel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Charts.Domain.ChartAggregate;
+using Charts.Domain.Fields;
+using Charts.Domain.DashboardAggregate;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Charts.Infrastructure.Repositories
+{
+    public class ChartRepository : IBaseChartRepository
+    {
+
+        private readonly ChartContext _context;
+
+        public ChartRepository(ChartContext context)
+        {
+
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public void Add(BaseChart chart)
+        {
+            _context.ChartElements.Add(chart);
+
+            //FieldType  enums data should be already present inside database
+            //added during seed with CharContext Seed
+            foreach (var entityEntry in _context.ChangeTracker.Entries<FieldType>())
+            {
+                entityEntry.State = EntityState.Unchanged;
+            }
+        }
+
+
+        private async Task LoadFieldTypeAsync()
+        {
+            if (_context.FieldTypes.Local.Count == 0)
+                await _context.FieldTypes.ToListAsync();
+        }
+
+        public async Task<BaseChart> FindByIdAsync(int id)
+        {
+            await LoadFieldTypeAsync();
+
+            var chart = await _context
+                        .ChartElements
+                        .Include(c => c.FieldGroups)
+                            .ThenInclude(a => a.Fields)
+                               .ThenInclude(a => a.Mappers)
+                                    .ThenInclude(a => a.OriginFieldType)
+                        .SingleOrDefaultAsync(a => a.Id == id);
+            return chart;
+        }
+
+        public void Remove(BaseChart chart)
+        {
+            _context.ChartElements.Remove(chart);
+            _context.FieldGroups.RemoveRange(chart.FieldGroups);
+            _context.Fields.RemoveRange(chart.FieldGroups.SelectMany(a => a.Fields));
+            _context.Mappers.RemoveRange(chart.FieldGroups.SelectMany(a => a.Fields.SelectMany( m => m.Mappers)));
+        }
+    }
+}
---
+++ DashboardRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Charts.Domain.DashboardAggregate;
+using Domain.SharedKernel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Charts.Infrastructure.Repositories
+{
+    public class DashboardRepository : IDashBoardRepository
+    {
+        private readonly ChartContext _context;
+
+        public DashboardRepository(ChartContext context)
+        {
+
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+
+        private async Task LoadResponsiveSizeAsync()
+        {
+            if (_context.Sizes.Local.Count == 0)
+                await _context.Sizes.ToListAsync();
+        }
+
+
+
+        public async Task<Dashboard> FindByIdAsync(int id)
+        {
+            await LoadResponsiveSizeAsync();
+            var dash = await _context.Dashboards.FindAsync(id);
+
+            if (dash != null)
+            {
+                await LoadContainerAsync(id);
+            }
+
+            return dash;
+        }
+
+        private async Task LoadContainerAsync(int id)
+        {
+            var containers = await (
+                    from c in _context.Containers
+                        .Include(a => a.ChildContainers)
+                        .Include(a => a.ChartContainers)
+                    where EF.Property<int>(c, "DashboardId") == id
+                    select c).ToArrayAsync();
+        }
+
+
+        public void Add(Dashboard dashboard)
+        {
+            _context.Dashboards.Add(dashboard);
+
+            foreach (var entityEntry in _context.ChangeTracker.Entries<ResponsiveSize>())
+            {
+                entityEntry.State = EntityState.Unchanged;
+            }
+        }
+
+        public void Remove(Dashboard dashboard)
+        {
+            _context.Dashboards.Remove(dashboard);
+            //_context.FieldGroups.RemoveRange(chart.FieldGroups);
+            //_context.Fields.RemoveRange(chart.FieldGroups.SelectMany(a => a.Fields));
+
+            _context.Containers.RemoveRange(dashboard.Containers);
+            _context.ChartContainers.RemoveRange(dashboard.Containers.SelectMany(d=>d.ChartContainers));
+
+
+         }
+    }
+}
